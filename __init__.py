6d97c5b8ded1e1f44233acea0de0db9f3468707f
someshugar445 <Kamala@096>
2020-10-07 20:54:44 +0530

    added books.json flask bootstrap and styling to table

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request, current_app
 from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
+bootstrap = Bootstrap()
 
-bootstrap = Bootstrap(app)
+
+def create_app():
+    app = Flask(__name__)
+    bootstrap.init_app(app)
+    return app
--- app.py
+++ app.py
@@ -1,56 +1,55 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_bootstrap import Bootstrap
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
-app.debug = True
+import json
 
+bootstrap = Bootstrap()
+
+
+def create_app():
+    my_app = Flask(__name__)
+    bootstrap.init_app(my_app)
+    return my_app
+
+
+app = create_app()
 # set a 'SECRET_KEY' to enable the Flask session cookies
 app.config['SECRET_KEY'] = 'asdf'
 
 toolbar = DebugToolbarExtension(app)
-books = [
-    {'book_id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge'},
-    {'book_id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin'},
-    {'book_id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany'}
-]
 
 
-@app.route('/', methods=['GET'])
-def home():
-    books_dict = {'book_id': int,
+@app.route('/')
+def index():
+    books_dict = {'link': str,
                   'title': str,
                   'author': str}
+    with open('books.json') as f:
+        books = json.load(f)
     columnNames = books_dict.keys()
     return render_template('home.html', records=books, colnames=columnNames)
 
 
-@app.route('/books', methods=['GET'])
-def api_id(book_id):
-    if 'id' in request.args:
-        book_id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an book_id."
-    # Create an empty list for our results
-    results = []
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['book_id'] == book_id:
-            results.append(book)
-    return jsonify(results)
+@app.route('/', methods=['GET'])
+def search_books(title):
+    if request.form['submit_button'] == 'search_book':
+        with open('books.json') as f:
+            books = json.load(f)
+        # Create an empty list for our results
+        results = []
+        for book in books:
+            if book['title'] == title:
+                results.append(book)
+            else:
+                "ERROR: Book with given book_id is not found"
+        return jsonify(results)
 
 
 @app.errorhandler(404)
-def page_not_found(error):
-    return 'This page does not exist', 404
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(port=8080, debug=True)
